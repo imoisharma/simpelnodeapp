8ca643e82793018adbae185170442eb4b797858f
imoisharma@icloud.com <imoisharma@icloud.com>
2020-11-14 14:30:07 +1200

    Changes made in index.js file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+
+
 const http = require('http');
+
 const server = http.createServer((req, res) => {
-    if (req.url == '/'){
-        res.end('<h1> Welcome to Incedge</h1>');
+    if (req.url === '/') {
+        res.end('<h1>Hello, World</h1>');
     }
-})
+});
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
